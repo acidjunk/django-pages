171251239208147d8277a195d0514e8957c0bb01
brian <brian@heimens-media.nl>
2015-11-24 16:54:13 +0100

    New files

--- dcms/test test.py
+++ dcms/test test.py
@@ -1,29 +1,34 @@
 import unittest
-import testgrid as test
+from testgrid import Grid, GridCell
 
 
 class MyTest(unittest.TestCase):
-    def test_vol_grid(self):
-        my_cell = test.GridCell(0, 0, 16, 2)
-        my_grid = test.Grid()
-        my_grid.add_cell(my_cell)
-        print("grid should now be full")
-        my_cell = test.GridCell(0, 0, 16, 2)
+    def test_full_grid(self):
+        my_cell = GridCell(0, 0, 16, 2)
+        my_grid = Grid()
         my_grid.add_cell(my_cell)
 
-    def test_leeg_grid(self):
+        errors = 0
+        for row in my_grid._data:
+            for col in row:
+                if row[col] == 0:
+                    errors += 1
+
+        assert errors == 0
+
+    def test_empty_grid(self):
         print("cell should be too wide")
-        my_cell = test.GridCell(0, 0, 30, 1)
-        my_grid = test.Grid()
+        my_cell = GridCell(0, 0, 30, 1)
+        my_grid = Grid()
         my_grid.add_cell(my_cell)
 
-    def test_leeg_grid_2_cellen(self):
+    def test_empty_grid_2_cells(self):
         print("cell should fit")
-        my_cell = test.GridCell(0, 0, 14, 2)
-        my_grid = test.Grid()
+        my_cell = GridCell(0, 0, 14, 2)
+        my_grid = Grid()
         my_grid.add_cell(my_cell)
         print("cell shouldn't fit")
-        my_cell = test.GridCell(0, 0, 14, 2)
+        my_cell = GridCell(0, 0, 14, 2)
         my_grid.add_cell(my_cell)
 
 
@@ -31,8 +36,8 @@ class MyTest(unittest.TestCase):
     # TODO, je maakt een grid aan dat leeg is en je probeert een te brede cell te plaatsen
     # TODO, je maakt een grid aan dat leeg is en je probeert een cell te plaatsen die past, direct daarna probeer je nog een cell te plaatsen die niet meer past
 
-print("...")
+
+
 
 if __name__ == '__main__':
-    print("start")
-    MyTest.test_vol_grid(unittest.defaultTestLoader)
+    unittest.main()
--- dcms/testgrid.py
+++ dcms/testgrid.py
@@ -11,44 +11,69 @@ class GridCell(object):
         result=''
         for row in self.data:
             for col in row:
-                result = result + 'x'
-            result = result + '\n'
+                result += 'x'
+            result += '\n'
 
         return result
+
 
 class Grid(object):
     height = 2
     width = 16
 
     def __init__(self):
-        self._data = [[0 for x in range(self.height)] for x in range(self.width)]
+        self._data = [[0 for x in range(self.width)] for x in range(self.height)]
         print(self._data)
 
     def __str__(self):
         result = ''
         for k in range(self.height):
             for i in range(self.width):
-                result = result + '.'
-            result = result + '\n'
+                result += '.'
+            result += '\n'
         return result
 
-    def check_size(self, cell):
+    def is_place_able(self, cell):
         if (cell.horizontal_position + cell.horizontal_size) > self.width:
-            return True
-        else:
             return False
+        else:
+            return True
 
-    def check_override(self, cell):
+    def check_override(self, cell, save):
         objects_in_row = 0
         long_enough = False
+        print('all',self._data)
+        for row in self._data[cell.vertical_position]: #, range(cell.horizontal_position,
+                                                             #cell.horizontal_position+cell.horizontal_size):
+            print('row', row)
+            for col in range(cell.horizontal_position,cell.horizontal_position+cell.horizontal_size):
+                print('col',self._data[row][col])
 
-        for row in self._data:
-            for object in row:
-                if row[object] == 0:
+                if self._data[row][col] == 0:
                     if objects_in_row == cell.horizontal_size:
                         long_enough = True
+                        if save:
+                            for colp in range(cell.horizontal_position, cell.horizontal_position+cell.horizontal_size):
+                                col[colp] = 1
                     else:
-                        objects_in_row = objects_in_row + 1
+                        objects_in_row += 1
+                        # print(objects_in_row)
+                else:
+                    print("e1, 1", self._data[row][col])
+                    pass
+
+                #if col == [0]:
+                    #if objects_in_row == cell.horizontal_size:
+                       # long_enough = True
+                        #if save:
+                            #for colp in range(cell.horizontal_position, cell.horizontal_position+cell.horizontal_size):
+                                #col[colp] = 1
+                    #else:
+                        #objects_in_row += 1
+                        # print(objects_in_row)
+                #else:
+                    #print("e1, 1", col)
+                    #pass
 
         return long_enough
 
@@ -56,31 +81,48 @@ class Grid(object):
         objects_in_row = 0
         free_spaces = []
         for row in self._data:
-            for object in row:
-                if row[object] == 0:
+            for col in range(cell.horizontal_position, cell.horizontal_position+cell.horizontal_size):
+                if row[col] == 0:
                     if objects_in_row == cell.horizontal_size:
-                        free_spaces = free_spaces + row
+                        free_spaces += row
                         objects_in_row = 0
                     else:
-                        objects_in_row = objects_in_row + 1
+                        objects_in_row += 1
 
         return free_spaces
 
     def add_cell(self, cell):
-        if self.check_size(cell) == False: #false = fits
-            if self.check_override(cell) == True:
-                #fits...
-                self._data = self._data + [cell]
+        if self.is_place_able(cell):  # false = fits
+            if self.check_override(cell, False):
+                # fits...
+                # TODO add specific location for cells? and not randomly assigned
+                # cell fits, recall the function and save it.
+                self.check_override(cell, True)
+
                 pass
             else:
                 print("cell overrides another")
         else:
             print("cell is too big")
 
-#TODO, Tests....
-#if __name__ == '__main__':
-   # my_cell = GridCell(0, 0, 16, 2)
-   # print(my_cell)
-  #  my_grid = Grid()
-   # print(my_grid)
-   # my_grid.add_cell(my_cell)
+    def move_cell(self, cell):
+        if self.check_override(cell, False):
+            if self.check_override(cell, False):
+                # fits...
+                # TODO add specific location for cells? and not randomly assigned
+                # cell fits, recall the function and save it.
+                self.check_override(cell, True)
+
+        else:
+            print("cell does not fit somewhere else")
+
+
+# TODO, Tests....
+if __name__ == '__main__':
+    my_cell = GridCell(0, 0, 16, 1) # H - V / Hs - Vs
+    my_grid = Grid()
+    my_grid.add_cell(my_cell)
+    print("grid should now be full")
+    my_cell2 = GridCell(0, 0, 16, 1) # H - V / Hs - Vs
+    my_grid.add_cell(my_cell2)
+    print(my_grid)
