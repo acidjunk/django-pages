8a5d3744533b971e55d68a6a557b2e2f599eab58
Rene Dohmen <rene@formatics.nl>
2015-12-22 09:50:22 +0100

    Added setting + working admin again + template tag for nav

--- demo_site/urls.py
+++ demo_site/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import include, url
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns(
+    '',
+    url(r'', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^', include('demo_site.pages.urls')),
-]
+)
+
