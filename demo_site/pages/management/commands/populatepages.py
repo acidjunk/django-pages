6e01e38891bf031d6eaec7d06e573dde733bc42c
Rene Dohmen <acidjunk@gmail.com>
2015-12-22 17:39:12 +0100

    Populate GRID with some random values

--- demo_site/pages/management/commands/populatepages.py
+++ demo_site/pages/management/commands/populatepages.py
@@ -1,12 +1,14 @@
 """
 A management command to populate the pages with some test content
 """
+import random
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.db.models import Q
 
-from ...models import Page, PageArticle, PageFAQ, PageLink, PageFacebookLink, PageYoutubeLink
+from ...models import Page, PageArticle, PageFAQ, PageLink, PageFacebookLink, PageYoutubeLink, GridCell
 
 class Command(BaseCommand):
     """
@@ -30,6 +32,27 @@ class Command(BaseCommand):
             order_number = 0
         return order_number
 
+    def _create_random_grid_row(self, page, vertical_position):
+        # Todo: rewrite to use Brian Grid validator (from the model)
+        cols = random.randint(1, 5)  # 1-4 cols
+        done = False
+        while not done:
+            sizes = random.sample(range(1, 17), cols)
+            if sum(sizes) <= 16:
+                done = True
+
+        for i in range(1, cols):
+            pos = 0
+            cell = GridCell()
+            cell.page = page
+            cell.horizontalPosition = pos
+            cell.horizontalSize = sizes.pop()
+            pos += cell.horizontalSize
+            cell.verticalPosition = vertical_position
+            cell.verticalSize = 1
+            cell.save()
+            i += 1
+
     def create_page(self, slug, name, slogan='', summary='', parent_page=None, sidebar_right=True):
         """
         This method generates a single page
@@ -46,7 +69,14 @@ class Command(BaseCommand):
         page.created_by = self.user
         page.modified_by = self.user
         page.save()
+
+        # generate some fake/random grid content
+        rows = random.randint(1, 10)  # 1-4 cols
+        for i in range(1, rows):
+            self._create_random_grid_row(page, i)
+            i += 1
         return page
+
 
     def handle(self, *args, **options):
         """
